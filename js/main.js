ae447b41e18d75bc6075480730bbf8d4815fec9c
MalcolmSmalls <smallsbeats@gmail.com>
2022-10-03 12:52:14 -0700

    Search button updates picture

--- js/main.js
+++ js/main.js
@@ -4,6 +4,13 @@ searchVal = "dance"
 let searchBtn = document.querySelector('#searchBtn')
 searchBtn.addEventListener('click', () => {
     searchVal = searchInput.value
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=kNz4rhxdiTEePFuPQJXKYZltwUB2S8ae&q=${searchVal}`)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response){
+        img.src = response.data[Math.floor(Math.random() * 50)].images.original.url
+    })
 })
 
 const img = document.querySelector('img')
