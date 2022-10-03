02408ae27f75953a250fe18a9c530215db626342
MalcolmSmalls <smallsbeats@gmail.com>
2022-10-03 12:00:19 -0700

    Add click button for randomized dances

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 const img = document.querySelector('img')
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=kNz4rhxdiTEePFuPQJXKYZltwUB2S8ae&s=dance')
+fetch('https://api.giphy.com/v1/gifs/search?api_key=kNz4rhxdiTEePFuPQJXKYZltwUB2S8ae&q=dance')
     .then(function(response){
         return response.json();
     })
     .then(function(response){
-        img.src = response.data.images.original.url
+        img.src = response.data[Math.floor(Math.random() * 50)].images.original.url
     })
+
+const switchBtn = document.querySelector('button')
+
+switchBtn.addEventListener('click', ()=> {
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=kNz4rhxdiTEePFuPQJXKYZltwUB2S8ae&q=dance')
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response){
+        img.src = response.data[Math.floor(Math.random() * 50)].images.original.url
+    })
+})
